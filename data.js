a7d9ba36b77a09a735a1bdbcdb0992b9c0b0f938
Transpordiportaal <205529323+Transpordiportaal@users.noreply.github.com>
2025-03-30 18:19:52 +0300

    initial setup

---
+++ data.js
@@ -0,0 +1,16 @@
+const dataColumns = [
+    { title: 'Väljumine' },
+    { title: 'Lähteriik' },
+    { title: 'Lähtekoht' },
+    { title: 'Sihtriik.' },
+    { title: 'Sihtkoht' },
+    { title: 'Lastiruum' },
+    { title: 'Kaal' },
+    { title: 'Haagisetüüp' }
+]
+
+const dataSet = [
+    ["01-01-2020","Eesti","Tallinn","Läti","Riga","2.4","4.8","Boksitreiler"],
+    ["12-10-2021","Eesti","Tartu","Soome","Helsinki","4.5","5.9","Eurotreiler"],
+    ["12-10-2021","Soome","Helsinki","Rootsi","Stockholm","1.5","4.2","Eurotreiler"],
+]
